6ffe9c1e3520f5a5d7ade1c2cdd1dd399989bf4b
Niha-21 <nihakamalnk@gmail.com>
2025-03-14 23:13:51 +0530

    created initial structure

---
+++ emscrud/src/test/java/com/emscrud/emscrud/EmscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emscrud.emscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
